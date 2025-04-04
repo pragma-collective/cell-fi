5c93b4b755faeb62acd0a405b1caaf5cde0a4d67
Alexis Banaag Jr. <aabanaag@gmail.com>
2025-04-05 00:01:29 +0800

    fix: update schema, remove wallet schema

--- api/src/db/schema/user.ts
+++ api/src/db/schema/user.ts
@@ -5,8 +5,9 @@ export const user = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   username: varchar("username", { length: 255 }).notNull(),
   ensName: varchar("ens_name", { length: 255 }).notNull(),
-  firstName: varchar("first_name", { length: 255 }).notNull(),
-  lastName: varchar("last_name", { length: 255 }).notNull(),
-  phoneNumber: varchar("phone_number", { length: 255 }).notNull(),
+  firstName: varchar("first_name", { length: 255 }),
+  lastName: varchar("last_name", { length: 255 }),
+  phoneNumber: varchar("phone_number", { length: 255 }),
+  walletAddress: varchar("wallet_address", { length: 255 }),
   ...timestamps,
 });
