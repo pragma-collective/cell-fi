173fcebe64bacc4989ef01b35f86fb9e810d6d97
Alexis Banaag Jr. <aabanaag@gmail.com>
2025-04-05 00:34:09 +0800

    feat: add transaction schema

---
+++ api/src/db/schema/transaction.ts
@@ -0,0 +1,21 @@
+import { pgTable, varchar, uuid, integer, pgEnum } from "drizzle-orm/pg-core";
+import { timestamps } from "./helper";
+import { user } from "./user";
+
+export const transactionStatus = pgEnum("transaction_status", [
+  "pending",
+  "success",
+  "failed",
+]);
+
+export const transactionType = pgEnum("transaction_type", ["send", "receive"]);
+
+export const transaction = pgTable("transactions", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  userId: uuid("user_id").references(() => user.id),
+  type: transactionType("type").notNull(),
+  txHash: varchar("tx_hash", { length: 255 }).notNull(),
+  status: transactionStatus("status").notNull(),
+  amount: integer("amount").notNull(),
+  ...timestamps,
+});
