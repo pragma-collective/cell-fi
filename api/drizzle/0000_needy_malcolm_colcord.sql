b39b9546534d74cdf781bcbd380f3560f7f964a3
Alexis Banaag Jr. <aabanaag@gmail.com>
2025-04-05 02:52:43 +0800

    fix: redo drizzle migration; update docker-compose to run migration

--- api/drizzle/0000_needy_malcolm_colcord.sql
+++ api/drizzle/0000_needy_malcolm_colcord.sql
@@ -1,8 +1,21 @@
 CREATE TYPE "public"."transaction_status" AS ENUM('pending', 'success', 'failed');--> statement-breakpoint
 CREATE TYPE "public"."transaction_type" AS ENUM('send', 'receive');--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"username" varchar(255) NOT NULL,
+	"ens_name" varchar(255) NOT NULL,
+	"first_name" varchar(255),
+	"last_name" varchar(255),
+	"phone_number" varchar(255),
+	"wallet_address" varchar(255) NOT NULL,
+	"updated_at" timestamp,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp
+);
+--> statement-breakpoint
 CREATE TABLE "transactions" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user_id" uuid,
+	"user_id" uuid NOT NULL,
 	"type" "transaction_type" NOT NULL,
 	"tx_hash" varchar(255) NOT NULL,
 	"status" "transaction_status" NOT NULL,
--- api/drizzle/0004_acoustic_mandroid.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "wallet_address" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "transactions" ALTER COLUMN "user_id" SET NOT NULL;
