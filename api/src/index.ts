3fe40420e438f253f9dc1fad7113756b0ff28f77
Albert Joseph Miclat <albertjmiclat@gmail.com>
2025-04-05 22:32:17 +0800

    feat: added authenticate /me endpoint (#20)

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,24 @@
 import { Hono } from "hono";
+import { jwt } from "hono/jwt";
 import { webhook } from "./api/webhook";
 import userRouter from "./routes/user";
 import authRouter from "./routes/auth";
+import meRouter from "./routes/me";
+import { JWT_SECRET } from "./util/constants";
 
 const app = new Hono();
+
+app.use(
+  '/me/*',
+  jwt({
+    secret: JWT_SECRET,
+  })
+)
 
 // Mount the routers
 app.route("/user", userRouter);
 app.route("/auth", authRouter);
+app.route("/me", meRouter);
 
 app.get("/", async (c) => {
   return c.json({
