6884acc13b24ff4faa626e934c4fffe582c659c4
Albert Miclat <albertjmiclat@gmail.com>
2025-04-05 15:16:47 +0800

    fix: blockchain name def

--- api/src/util/constants.ts
+++ api/src/util/constants.ts
@@ -1,5 +1,3 @@
-import { Blockchain } from '@circle-fin/developer-controlled-wallets';
-
-export const BLOCKCHAIN_ID = Blockchain.ArbSepolia;
+export const BLOCKCHAIN_ID = 'ARB-SEPOLIA';//  Blockchain.ArbSepolia;
 export const CIRCLE_ACCOUNT_TYPE = "SCA";
 export const ENS_DOMAIN = "cellfi.eth";
