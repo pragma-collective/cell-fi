073a774dba2e08751b390f34eb19a767ee249a19
Nicko Picazo <nickopicazo@gmail.com>
2025-04-06 03:07:24 +0800

    updated branding and indentity

--- web/src/app/login/hero.jsx
+++ web/src/app/login/hero.jsx
@@ -4,7 +4,8 @@ export default function Hero() {
   return (
     <div className="bg-white rounded-[10px] overflow-hidden relative after:content-[''] after:absolute after:inset-0 after:bg-[var(--color-secondary)] after:opacity-50 after:mix-blend-lightenn after:z-10 flex items-center justify-center aspect-[3/2] md:aspect-auto">
         <Image src="/bg-login.jpg" width={6000} height={4000} className="d-block absolute inset-0 z-0 h-full w-full object-cover" alt="background" />
-        <div className="absolute bottom-5 left-5 right-5 z-30 text-[20px] md:text-[30px] text-right leading-tight font-extralight">Your Financial Bridge, <br />Always Connected.</div>
+        <div className="absolute bottom-5 left-5 right-5 z-30 text-[20px] md:text-[30px] text-right leading-tight font-extralight">SMS-based peer-to-peer payments using <br />
+        USDC digital currency - no internet required.</div>
         <div className="text-white relative z-40 w-[150px]">
         <Image src="/logo.svg" width={250} height={55} className="w-[250px] h-auto" alt="logo" />
         </div>
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,9 +6,11 @@ export default function Home() {
     <div className="h-full">
       <div className="bg-white overflow-hidden relative after:content-[''] after:absolute after:inset-0 after:bg-[var(--color-secondary)] after:opacity-50 after:mix-blend-lightenn after:z-10 flex items-center justify-center aspect-[3/2] md:aspect-auto h-full">
         <Image src="/bg-login.jpg" width={6000} height={4000} className="d-block absolute inset-0 z-0 h-full w-full object-cover" alt="background" />
-        <div className="absolute bottom-5 left-5 right-5 z-30 text-[20px] md:text-[30px] text-right leading-tight font-extralight">Your Financial Bridge, <br />Always Connected.</div>
+        <div className="absolute bottom-5 left-5 right-5 z-30 text-[20px] md:text-[30px] text-right leading-tight font-extralight">SMS-based peer-to-peer payments using <br />
+        USDC digital currency - no internet required.</div>
         <div className="text-white relative z-40 w-[150px]">
-        <Image src="/logo.svg" width={250} height={55} className="w-[250px] h-auto mb-10" alt="logo" />
+        <Image src="/logo.svg" width={250} height={55} className="w-[250px] h-auto mb-2" alt="logo" />
+        <h3 className="font-bold mb-10 text-center tracking-[5px]">CellFi</h3>
         <Link href="/login" className="button button--primary whitespace-nowrap !px-10">Login &nbsp;→</Link>
         </div>
       </div>
